93b4c8d687420adcfffe00cc5f329fe305d25b94
rbust0 <brett.bushman@gmail.com>
2020-04-25 08:57:37 -0500

    First real work on it that wasnt just framework.

---
+++ pages/FistToFive.ts
@@ -0,0 +1,10 @@
+import  {IDecision, IDecisions}  from './decision';
+
+export const FistToFive : IDecision[] = [
+          {value: "0", description:'Blocked. I hate this'},
+          {value: "1", description: 'Major Conerns. Stop being dumb'},
+          {value: "2", description: "Getting onboard, still some concerns"},
+          {value: "3", description: "Bought in, but I have reversations"},
+          {value: "4", description: "Absolutely, let's do this"},
+          {value: "5", description: "OMG THIS IS THE BEST THING EVER CAN I PLEASE LEAD IT"}
+    ];
---
+++ pages/decision.ts
@@ -0,0 +1,9 @@
+export interface IDecision {
+    value: string,
+    description: string;
+
+};
+
+export interface IDecisions {
+    decisions: IDecision[];
+}
